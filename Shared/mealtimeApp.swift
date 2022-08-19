fd2540538b378f30b7f57f5a56823cd9718e4449
Sungho Moon <moon7959@kakao.com>
2022-08-19 14:57:29 +0900

    Initial Commit

---
+++ Shared/mealtimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealtimeApp.swift
+//  Shared
+//
+//  Created by 문성호 on 2022/08/19.
+//
+
+import SwiftUI
+
+@main
+struct mealtimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
